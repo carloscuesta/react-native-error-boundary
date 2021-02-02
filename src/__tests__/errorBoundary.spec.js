2845bbf4fff7f1255dcdc1d57d9e828c446970a5
Carlos Cuesta <carloscuesta@me.com>
2021-02-02 12:32:20 +0100

    :white_check_mark: Update tests suites and devDependencies

--- src/__tests__/errorBoundary.spec.js
+++ src/__tests__/errorBoundary.spec.js
@@ -24,49 +24,71 @@ describe('ErrorBoundary', () => {
     })
   })
 
-  it('should render children when there are no errors', () => {
-    const wrapper = renderer.create(
-      <ErrorBoundary>
-        <Text>Hey!</Text>
-      </ErrorBoundary>
-    )
-    expect(wrapper.getInstance().state.error).toBe(null)
-    expect(wrapper).toMatchSnapshot()
+  describe('when there are no errors', () => {
+    it('should render the children', () => {
+      const wrapper = renderer.create(
+        <ErrorBoundary>
+          <Text>Hey!</Text>
+        </ErrorBoundary>
+      )
+
+      expect(wrapper).toMatchSnapshot()
+    })
   })
 
-  it('should catch errors set the state and render the default FallbackComponent', () => {
-    const wrapper = renderer.create(
-      <ErrorBoundary>
-        <View>{errorMock}</View>
-      </ErrorBoundary>
-    )
-    expect(wrapper.getInstance().state.error).toBeInstanceOf(Error)
-    expect(wrapper.getInstance().state.error).not.toBeNull()
-    expect(wrapper).toMatchSnapshot()
-  })
+  describe('when there is an error', () => {
+    describe('when FallbackComponent is not defined as a prop', () => {
+      it('should catch the error and render the default FallbackComponent', () => {
+        const wrapper = renderer.create(
+          <ErrorBoundary>
+            <View>{errorMock}</View>
+          </ErrorBoundary>
+        )
 
-  it('should catch errors set the state render the props.FallbackComponent and call props.onError', () => {
-    const FallbackComponent = () => <Text>Error!</Text>
-    const onError = jest.fn()
-    const wrapper = renderer.create(
-      <ErrorBoundary FallbackComponent={FallbackComponent} onError={onError}>
-        <View>{errorMock}</View>
-      </ErrorBoundary>
-    )
-    expect(wrapper.getInstance().state.error).toBeInstanceOf(Error)
-    expect(wrapper.getInstance().state.error).not.toBeNull()
-    expect(onError).toHaveBeenCalledWith(expect.any(Error), expect.any(String))
-    expect(wrapper).toMatchSnapshot()
-  })
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
 
-  it('should clear state when calling resetError', () => {
-    const wrapper = renderer.create(
-      <ErrorBoundary>
-        <Text>No errors!</Text>
-      </ErrorBoundary>
-    )
-    wrapper.getInstance().setState({ error: errorMock })
-    wrapper.getInstance().resetError()
-    expect(wrapper.getInstance().state.error).toBeNull()
+    describe('when FallbackComponent is defined as a prop', () => {
+      it('should catch the error and render the props.FallbackComponent', () => {
+        const FallbackComponent = () => <Text>Error!</Text>
+        const wrapper = renderer.create(
+          <ErrorBoundary FallbackComponent={FallbackComponent}>
+            <View>{errorMock}</View>
+          </ErrorBoundary>
+        )
+
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
+
+    describe('when onError is defined as a prop', () => {
+      it('should catch the error and call props.onError', () => {
+        const onError = jest.fn()
+
+        renderer.create(
+          <ErrorBoundary onError={onError}>
+            <View>{errorMock}</View>
+          </ErrorBoundary>
+        )
+
+        expect(onError).toHaveBeenCalledWith(expect.any(Error), expect.any(String))
+      })
+    })
+
+    describe('when FallbackComponent resetError prop is called', () => {
+      it('should clear the error state', () => {
+        const wrapper = renderer.create(
+          <ErrorBoundary>
+            <Text>No errors!</Text>
+          </ErrorBoundary>
+        )
+
+        wrapper.getInstance().setState({ error: errorMock })
+        wrapper.getInstance().resetError()
+
+        expect(wrapper.getInstance().state.error).toBeNull()
+      })
+    })
   })
 })
