61d2ab88bc905edb8731dba3153c904c94ef3f1a
Carlos Cuesta <carloscuesta@me.com>
2026-06-28 10:50:47 +0200

    ♻️ Fix jest build

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from 'jest'
 
 const config = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.(ts|tsx)'],
   modulePathIgnorePatterns: ['<rootDir>/lib'],
   reporters: ['default', 'github-actions'],
+  testEnvironment: 'node',
+  testEnvironmentOptions: {
+    customExportConditions: ['require', 'react-native'],
+  },
   transformIgnorePatterns: [],
 } satisfies Config
 
